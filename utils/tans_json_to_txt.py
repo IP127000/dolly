4550697faf0cfc460350624e26735d93b609cc85
coffee <hanluzhi0429@gmail.com>
2025-04-29 18:28:45 +0800

    add utils for data process

--- utils/tans_json_to_txt.py
+++ utils/tans_json_to_txt.py
@@ -11,3 +11,4 @@ def extract_completions(json_file_path, output_file_path):
             output_file.write(completion + '\n')
 
 extract_completions('corpus/wikipedia-cn-20230720-filtered.json', 'corpus/wikipedia.txt')
+
