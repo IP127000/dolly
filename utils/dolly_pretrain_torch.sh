acdbde74cea010cac02d7078afd757a5618e938d
coffee <hanluzhi0429@gmail.com>
2025-05-19 11:52:16 +0800

    add torchrun train script

---
+++ utils/dolly_pretrain_torch.sh
@@ -0,0 +1,3 @@
+python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=12345 src_pretrain/pretrain_transformers.py
+# torchrun --nproc_per_node=4 src_pretrain/pretrain_transformers.py
+# CUDA_VISIBLE_DEVICES=1,2,3 torchrun --nproc_per_node=3 src_pretrain/pretrain_transformers.py
