ef2f7a1bc59a54ab54fdb26144f8c620e37c4207
coffee <hanluzhi0429@gmail.com>
2025-06-10 13:58:12 +0800

    [25.06.10] update code

--- src_inference/dolly_inference.py
+++ src_inference/dolly_inference.py
@@ -1,8 +1,5 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model_name = "Qwen/Qwen3-8B"
-
-# load the tokenizer and the model
+model_name = ""
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
@@ -10,7 +7,7 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto"
 )
 
-# prepare the model input
+
 prompt = "Give me a short introduction to large language model."
 messages = [
     {"role": "user", "content": prompt}
@@ -19,20 +16,18 @@ text = tokenizer.apply_chat_template(
     messages,
     tokenize=False,
     add_generation_prompt=True,
-    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+    enable_thinking=True 
 )
 model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-# conduct text completion
 generated_ids = model.generate(
     **model_inputs,
     max_new_tokens=32768
 )
 output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
 
-# parsing thinking content
+
 try:
-    # rindex finding 151668 (</think>)
     index = len(output_ids) - output_ids[::-1].index(151668)
 except ValueError:
     index = 0
